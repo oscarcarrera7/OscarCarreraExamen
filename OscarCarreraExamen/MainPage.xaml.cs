c16a69893fa16a04ebc5bdedf498419a2d931592
Oscar Carrera <ocarrera_electric@hotmail.com>
2021-04-27 23:08:34 -0500

    Agregar archivos de proyecto.

---
+++ OscarCarreraExamen/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OscarCarreraExamen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            String user = txtUser.Text;
+            String pass = txtPass.Text;
+
+            if ((user == "estudiante2021") && (pass == "uisrael2021"))
+            {
+                await Navigation.PushAsync(new registro(user, pass));
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario o contraseña equivocados", "Gracias");
+            }
+
+        }
+    }
+}
---
+++ OscarCarreraExamen/registro.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OscarCarreraExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro(String user, String pass)
+        {
+            InitializeComponent();
+            lblUser.Text = "Usuario conectado: " + user;
+            
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            double monto = Convert.ToDouble(txtMonto.Text);
+
+            double inicial = (1800 - monto);
+            double pagoin = (inicial / 3);
+            double saldo = pagoin + 90;
+
+            txtPago.Text = saldo.ToString();
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            
+            String nombre = txtNombre.Text;
+            String user = lblUser.Text;
+            String pago = txtPago.Text;
+
+            DisplayAlert("Mensaje", "Elemento Guardado con exito", "Gracias");
+
+            await Navigation.PushAsync(new resumen(nombre, user, pago));
+        }
+    }
+}
